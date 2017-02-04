ad54513a00432d95bbab81aad8ca5def0ce2b94c
Lxx <779219930@qq.com>
2017-02-04 14:18:58 +0800

    地信红盟主页

---
+++ application/home/model/Redforum.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lxx<779219930@qq.com>
+ * Date: 2017/2/4
+ * Time: 12:42
+ */
+
+namespace app\home\model;
+
+
+use think\Model;
+
+class Redforum extends Model {
+
+}
