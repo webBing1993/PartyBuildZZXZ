a7c0a7d436ad388393cac07481bd04fddbf12048
yangmingzhe <15700004138@163.com>
2017-04-07 10:42:40 +0800

    智慧拼图 (后台前端页面加后端)

---
+++ application/admin/model/GameImg.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/1
+ * Time: 10:47
+ */
+namespace app\admin\model;
+use think\Model;
+class GameImg extends Model{
+    
+}
