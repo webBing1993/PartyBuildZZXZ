866aa906430956e3a103a1b28ddc39361cded6d7
ben <750405122@qq.com>
2018-01-12 19:08:09 +0800

    组织架构

--- application/home/controller/Service.php
+++ application/home/controller/Service.php
@@ -7,16 +7,40 @@
  */
 
 namespace app\home\controller;
-
+use app\home\model\WechatDepartmentUser;
+use app\home\model\WechatUser;
+use app\home\model\WechatDepartment;
+use app\home\model\WechatUserTag;
 
 class Service extends Base
 {
     public function organization(){
+        $list = WechatDepartment::where(['parentid' => 1])->order('id')->select();
+        $this->assign('list',$list);
         return $this->fetch();
     }
     public function ordetail(){
+        $this ->checkAnonymous();
+        $party = input('id');
+        $departmentName = WechatDepartment::where(['id' => $party])->order('id')->value('name');
+        $modelAll = WechatDepartmentUser::where(['departmentid' => $party])->order('id')->select();
+//        $bg_color=["#b1e3fc", "#aeefef", "#ffa351", "#9393f5", "#cf88f7", "#65abfa", "#ebcffb", "#76f4f0", "#ffcf6e", "#ff8ff4"];
+        foreach ($modelAll as $k => $model){
+            $tag = WechatUserTag::where(['userid' => $model['userid'], 'tagid' => 1])->find();
+            if(!$tag){
+                unset($modelAll[$k]);
+                continue;
+            }
+            $model['name'] = WechatUser::where(['userid' => $model['userid']])->value('name');
+            $model['surname'] = mb_substr($model['name'], 0, 1,'utf-8');
+//            $model['color'] = $bg_color[substr($model['mobile'], 7, 1)];
+        }
+//        var_dump($modelAll);die;
+        $this->assign('departmentName',$departmentName);
+        $this->assign('modelAll',$modelAll);
         return $this->fetch();
     }
+
     public function manage(){
         return $this->fetch();
     }
