43c28abc85e35fe22c7eb80c8e4d5a6ad40f1f71
stiff <15700004138@163.com>
2018-01-26 11:19:03 +0800

    党员展示 按照党员姓名首字符排序

--- application/home/controller/Service.php
+++ application/home/controller/Service.php
@@ -21,6 +21,8 @@ use think\Db;
 class Service extends Base
 {
     static $MEMBER_TAG = 1; // 党员标签ID
+    static $DEFAUL_AVATAR = ''; // 用户默认头像
+
     /**
      * 党员管理
      */
@@ -55,11 +57,94 @@ class Service extends Base
                 ->join('pb_wechat_department_user b','a.userid = b.userid')
                 ->join('pb_wechat_user_tag c','a.userid = c.userid')
                 ->join('pb_wechat_department d','b.departmentid = d.id')
-                ->field('d.name as dname,a.userid,a.name,a.mobile,a.avatar,a.position')
+                ->field('d.name as dname,a.userid,a.name,a.mobile,a.avatar,a.header,a.position')
                 ->where(['departmentid'=>$did,'tagid'=>$this::$MEMBER_TAG])
                 ->select();
 
-            return $this->success('获取成功！',null,json_encode($list));
+            $charArray = [];//将姓名按照姓的首字母与相对的首字母键进行配对
+            foreach ($list as $user) {
+                $name = $user['name'];
+                if (!empty($user['header'])) { // 获取用户头像
+
+                    $img = Picture::get($user['header']);
+                    $user['header'] = $img['path'];
+                } else if (empty($user['avatar'])) { // 无头像设置默认
+
+                    $user['header'] = $this::$DEFAUL_AVATAR;
+                }
+
+                $char = $this->getFirstChar($name);
+                $nameArray = [];
+
+                if (isset($charArray[$char]) && count($charArray[$char])!=0) {
+                    $nameArray = $charArray[$char];
+                }
+                array_push($nameArray,$user);
+                $charArray[$char] = $nameArray;
+            }
+            ksort($charArray);
+//            return json_encode($charArray);
+            return $this->success('获取成功！',null,json_encode($charArray));
+        }
+    }
+
+    /**
+     * 首字符排序
+     * @param $s 用户姓名
+     * @return bool|string
+     */
+    protected function getFirstChar($s)
+    {
+        $s0 = mb_substr($s,0,3); // 获取名字的姓
+        $s = iconv('UTF-8','gb2312', $s0); // 将UTF-8转换成GB2312编码
+        if (ord($s0)>128) { // 汉字开头，汉字没有以U、V开头的
+            $asc=ord($s{0})*256+ord($s{1})-65536;
+            if($asc>=-20319 and $asc<=-20284)return "A";
+            if($asc>=-20283 and $asc<=-19776)return "B";
+            if($asc>=-19775 and $asc<=-19219)return "C";
+            if($asc>=-19218 and $asc<=-18711)return "D";
+            if($asc>=-18710 and $asc<=-18527)return "E";
+            if($asc>=-18526 and $asc<=-18240)return "F";
+            if($asc>=-18239 and $asc<=-17760)return "G";
+            if($asc>=-17759 and $asc<=-17248)return "H";
+            if($asc>=-17247 and $asc<=-17418)return "I";
+            if($asc>=-17417 and $asc<=-16475)return "J";
+            if($asc>=-16474 and $asc<=-16213)return "K";
+            if($asc>=-16212 and $asc<=-15641)return "L";
+            if($asc>=-15640 and $asc<=-15166)return "M";
+            if($asc>=-15165 and $asc<=-14923)return "N";
+            if($asc>=-14922 and $asc<=-14915)return "O";
+            if($asc>=-14914 and $asc<=-14631)return "P";
+            if($asc>=-14630 and $asc<=-14150)return "Q";
+            if($asc>=-14149 and $asc<=-14091)return "R";
+            if($asc>=-14090 and $asc<=-13319)return "S";
+            if($asc>=-13318 and $asc<=-12839)return "T";
+            if($asc>=-12838 and $asc<=-12557)return "W";
+            if($asc>=-12556 and $asc<=-11848)return "X";
+            if($asc>=-11847 and $asc<=-11056)return "Y";
+            if($asc>=-11055 and $asc<=-10247)return "Z";
+        } else if (ord($s)>=48 and ord($s)<=57) { // 数字开头
+            switch(iconv_substr($s,0,1,'utf-8')){
+                case 1:return "Y";
+                case 2:return "E";
+                case 3:return "S";
+                case 4:return "S";
+                case 5:return "W";
+                case 6:return "L";
+                case 7:return "Q";
+                case 8:return "B";
+                case 9:return "J";
+                case 0:return "L";
+            }
+        } else if (ord($s)>=65 and ord($s)<=90) { // 大写英文开头
+
+            return substr($s,0,1);
+        } else if (ord($s)>=97 and ord($s)<=122) { // 小写英文开头
+
+            return strtoupper(substr($s,0,1));
+        } else { // 中英混合的词语，不适合上面的各种情况，因此直接提取首个字符即可
+
+            return iconv_substr($s0,0,1,'utf-8');
         }
     }
 
@@ -77,9 +162,20 @@ class Service extends Base
                 ->join('pb_wechat_department_user b','a.userid = b.userid','left')
                 ->join('pb_wechat_user_tag c','a.userid = c.userid','left')
                 ->join('pb_wechat_department d','b.departmentid = d.id','left')
-                ->field('d.name as dname,a.userid,a.name,a.mobile,a.avatar,a.position')
+                ->field('d.name as dname,a.userid,a.name,a.mobile,a.avatar,a.header,a.position')
                 ->where(['a.name'=>['like',"%$search%"],'tagid'=>$this::$MEMBER_TAG])
                 ->select();
+
+            foreach ($list as $user) {
+                if (!empty($user['header'])) { // 获取用户头像
+
+                    $img = Picture::get($user['header']);
+                    $user['header'] = $img['path'];
+                } else if (empty($user['avatar'])) { // 无头像设置默认
+
+                    $user['header'] = $this::$DEFAUL_AVATAR;
+                }
+            }
 
             return $this->success('搜索获取成功！',null,json_encode($list));
         }
@@ -160,4 +256,6 @@ class Service extends Base
 
         return $this->fetch();
     }
+
+
 }
