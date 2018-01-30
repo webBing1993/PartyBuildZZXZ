b72952a375b930bd3ee5145f83730be390e53af4
xjwmyy <13213102067@163.com>
2018-01-30 20:44:56 +0800

    bug

--- application/home/controller/Base.php
+++ application/home/controller/Base.php
@@ -61,10 +61,10 @@ class Base extends Controller {
 
     public function _initialize(){
 //        session('userId',null);
-//        session('userId','18767104335'); //申请人权限
+        session('userId','18767104335'); //申请人权限
 //        session('userId','15757118744'); //调解员1权限
 //        session('userId','18268031620'); //调解员2权限
-        session('userId','13857282889'); //管理员权限
+//        session('userId','13857282889'); //管理员权限
 
 //        session('header','/home/images/vistor.jpg');
 //        session('nickname','游客');
