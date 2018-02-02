271f0c28f58fa7c75052d13d318b20969b2a2c69
15558023165@163.com <zzb111004170407>
2018-02-02 16:59:37 +0800

    调解员管理学历

--- application/admin/validate/Mediationuser.php
+++ application/admin/validate/Mediationuser.php
@@ -28,7 +28,7 @@ class Mediationuser extends Validate {
         'description.require' =>  '请添加口号！',
         'birthday.require' =>  '请添加出生年月！',
        /* 'politics_status.require' =>  '请添加政治面貌！',*/
-        'education.require'  =>  '请填写学历！',
+        'education.require'  =>  '请选择学历！',
         'content.require'  =>  '请填写工作简历！',
     ];
 }
